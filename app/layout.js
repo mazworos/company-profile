efd3190d485a04415208f041a2e2dc6e65bf758f
mazworos <worosusanto02@gmail.com>
2024-08-10 18:24:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.js
@@ -0,0 +1,34 @@
+import { Sen } from "next/font/google";
+import "./globals.css";
+
+// Components
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+// Theme Provider
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const Josef = Sen({ 
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"],
+  display: "swap"
+});
+
+export const metadata = {
+  title: "Homellow",
+  description: "Home Architecture",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={Josef.className}>
+        <ThemeProvider attribute='class' defaultTheme='light' >
+          <Header />
+            {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
